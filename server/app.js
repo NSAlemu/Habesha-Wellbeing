3c82150973986049a1aa5a514935aecccf57f8d1
nsalemu <nsalemu@outlook.com>
2019-12-22 14:35:51 -0800

    test deploy 7

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use("/api/posts", posts);
 app.use("/api/sidenav", sidenav);
 
 if(process.env.NODE_ENV==='production'){
-  app/use(express.static(__dirname + "/public"));
+  app.use(express.static(__dirname + "/public"));
   app.get(/.*/, (req, res) => {
     //const posts = await loadPosts();
     res.sendFile(__dirname+'/public/index.html');
